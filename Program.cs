1c0c616af8da51ab831273e770071511112d06de
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-12 20:40:50 -0300

    Resolução da atividade ImitandoDory

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿﻿Console.WriteLine("---Dory---");
+
+Console.WriteLine("Escreva seu texto aqui:");
+string corro=Console.ReadLine()!;
+Console.WriteLine(corro.Replace("ã","ããã").Replace("é","ééé").Replace("í","ííí").Replace("ó","óóó").Replace("û","ûûû").Replace("a","aaa").Replace("e","eee").Replace("i","iii").Replace("o","ooo").Replace("u","uuu"));
